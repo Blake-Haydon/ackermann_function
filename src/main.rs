2b79c76cc8fc6d5ae7dd419cbbddf13f8f54a8f1
Blake Haydon <blake.a.haydon@gmail.com>
2022-05-29 16:49:44 +1000

    refactor: simplify printing of vars

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /// Ackermann Function as defined in [Wikipedia](https://en.wikipedia.org/wiki/Ackermann_function).
 /// The function is only valid for non-negative integers.
-pub fn ackermann(m: u32, n: u32) -> u32 {
+fn ackermann(m: u32, n: u32) -> u32 {
     match (m, n) {
         (0, n) => n + 1,
         (m, 0) => ackermann(m - 1, 1),
@@ -9,22 +9,18 @@ pub fn ackermann(m: u32, n: u32) -> u32 {
 }
 
 fn main() {
-    let m = 4;
-    let n = 1;
+    // Define m and n vars for the ackermann function
+    let m = 2;
+    let n = 3;
 
     // Time how long it takes to calculate the ackermann function with (4, 1)
     let start = std::time::Instant::now();
     let result = ackermann(m, n);
     let end = start.elapsed();
+    let millis = end.as_millis();
 
     // Print result
-    println!(
-        "ackermann(m={}, n={}) = {} in {} ms",
-        m,
-        n,
-        result,
-        end.as_millis(),
-    );
+    println!("ackermann(m={m}, n={n}) is equal to {result} in {millis} ms");
 }
 
 // Unit tests
